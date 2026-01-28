3494e81674d2bce16ac25be068ce5f894a80c8af
Vuong189 <vuonglmao1221@gmail.com>
2026-01-29 00:46:07 +0700

    test commit

---
+++ SWD.Business/DTOs/TestCommitDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWD.Business.DTOs
+{
+    public class TestCommitDTO
+    {
+        public Guid Id { get; set; }
+    }
+}
